60f366aeebe49c5d760c38b3a48822c87e2aa45a
cxzhong <chenxin.zhong@outlook.com>
2026-03-23 23:52:24 +0800

    Fix: Ensure scripts directory is created if it does not exist

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@
 robocopy . "%LIBRARY_LIB%\mathjax" /E /XD .github /XF .gitignore build_env_setup.bat build_env_setup.sh conda_build.bat conda_build.sh metadata_conda_debug.yaml
 if %ERRORLEVEL% GEQ 8 exit /B 1
 
+if not exist "%SCRIPTS%" mkdir "%SCRIPTS%"
 copy /Y "%RECIPE_DIR%\mathjax-path.bat" "%SCRIPTS%\"
 if errorlevel 1 exit /B 1
 copy /Y "%RECIPE_DIR%\mathjax-path" "%SCRIPTS%\"
