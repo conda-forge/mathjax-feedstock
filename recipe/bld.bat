a0561d18f149b00375e4b8439b684d9bf47cb536
cxzhong <chenxin.zhong@outlook.com>
2026-03-23 23:47:48 +0800

    Refactor build scripts to improve file handling and cleanup process

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,6 @@
 @echo off
 
-del /Q build_env_setup.bat conda_build.bat metadata_conda_debug.yaml 2>nul
-del /Q build_env_setup.sh conda_build.sh 2>nul
-
-robocopy . "%LIBRARY_LIB%\mathjax" /E /XD .github /XF .gitignore
+robocopy . "%LIBRARY_LIB%\mathjax" /E /XD .github /XF .gitignore build_env_setup.bat build_env_setup.sh conda_build.bat conda_build.sh metadata_conda_debug.yaml
 if %ERRORLEVEL% GEQ 8 exit /B 1
 
 copy /Y "%RECIPE_DIR%\mathjax-path.bat" "%SCRIPTS%\"
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,14 @@ mathjax="$PREFIX/lib/mathjax"
 bin_dir="$PREFIX/bin"
 
 mkdir -p "$mathjax"
-rm -f build_env_setup.sh conda_build.sh metadata_conda_debug.yaml
-cp -r ./* "$mathjax/"
+find . -maxdepth 1 \
+    ! -name '.' \
+    ! -name '.github' \
+    ! -name '.gitignore' \
+    ! -name 'build_env_setup.*' \
+    ! -name 'conda_build.*' \
+    ! -name 'metadata_conda_debug.yaml' \
+    -exec cp -r {} "$mathjax/" \;
 
 mkdir -p "${bin_dir}"
 cp "${RECIPE_DIR}/mathjax-path" "${bin_dir}/"
